e3c2b9812dfb537446dcb74f0ef1e02b77ff5a11
nikisim <nikisim2000@gmail.com>
2024-03-21 20:57:00 +0300

    fix qagent

--- src/qagent.py
+++ src/qagent.py
@@ -26,12 +26,44 @@ class QLearningAgent(object):
         self.min_epsilon = exploration_parameters["min_epsilon"]
         self.decay_rate = exploration_parameters["decay_rate"]
 
+    def get_qvalue(self, state, action):
+        """ Returns Q(state,action) """
+        return self.qtable[state,action]
+    
+    def set_qvalue(self, state, action, value):
+        """ Sets the Qvalue for [state,action] to the given value """
+        self.qtable[state,action] = value
+
+    def get_value(self, state: int, state_dict):
+        """
+        Compute your agent's estimate of V(s) using current q-values
+        V(s) = max_over_action Q(state,action) over possible actions.
+        Note: please take into account that q-values can be negative.
+        """
+        possible_actions = self.get_legal_actions(state)
+        key = list(filter(lambda x: state_dict[x] == state, state_dict))[0]
+        state_pure = np.array(key[:-1])
+        action_space = np.where(state_pure == 0)[0]
+
+        # If there are no legal actions, return 0.0
+        if len(action_space) == 0:
+            return 0.0
+
+        return max([self.get_qvalue(state, act) for act in action_space])
+
     def update_qtable(self, state, new_state, action, reward, done):
-        return self.qtable[state, action] + self.learning_rate * (
-            reward
-            + self.gamma * np.max(self.qtable[new_state, :]) * (1 - done)
-            - self.qtable[state, action]
-        )
+        """
+        Q-Value update:
+           Q(s,a) := (1 - alpha) * Q(s,a) + alpha * (r + gamma * V(s'))
+        """
+        # return self.qtable[state, action] + self.learning_rate * (
+        #     reward
+        #     + self.gamma * np.max(self.qtable[new_state, :]) * (1 - done)
+        #     - self.qtable[state, action])
+
+        reference_qvalue = reward + self.gamma * np.max(self.qtable[new_state, :])
+        updated_qvalue = self.learning_rate * reference_qvalue + (1 - self.learning_rate) * self.qtable[state, action]
+        return updated_qvalue
     
     def get_qtable(self):
         return self.qtable
